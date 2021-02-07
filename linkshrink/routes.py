3b7b481b842dc8da1ba566d46871788264f21c78
Alex Karlsson Lindén <65206220+alexkarlin@users.noreply.github.com>
2021-02-07 21:45:15 +0100

    Change route naming convention
    All route functions now end in '_route' to make them easily distinguishable from other functions.

--- linkshrink/routes.py
+++ linkshrink/routes.py
@@ -4,15 +4,15 @@ from . import database
 
 @current_app.route('/')
 @current_app.route('/index')
-def index():
+def index_route():
     return 'Hello, World!'
 
 # Responsible for redirecting the user to the target_url
 # associated with the specified shrunk_url.
 # Aborts a 404 if the speicifed shrunk_url does not exist.
 @current_app.route('/<shrunk_url>')
-def shrunk_url_redirect(shrunk_url):
-    url = database.get_target_url(shrunk_url)
+def redirect_route(shrunk_url):
+    url = database.query_target_url(shrunk_url)
 
     if url is not None:
         return redirect(url, code=302)
