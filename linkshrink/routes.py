f2e1df40c9421dead720211827ea2e63fbd34470
Alex Karlsson Lindén <65206220+alexkarlin@users.noreply.github.com>
2021-02-11 22:40:59 +0100

    Create placeholder front-end
    Created a very basic front-end to test the functionality of the website.

--- linkshrink/routes.py
+++ linkshrink/routes.py
@@ -1,11 +1,26 @@
-from flask import request, redirect, abort, current_app, g
+from flask import request, redirect, abort, current_app, g, render_template
+
+from . import shortener
 from . import database
 
 
-@current_app.route('/')
-@current_app.route('/index')
+@current_app.route('/', methods=['POST', 'GET'])
 def index_route():
-    return 'Hello, World!'
+    if (request.method == 'POST'):
+        print('POSTing @ index -- starting shrinking process')
+
+        target_url = request.form.get('target_url')
+
+        with current_app.app_context():
+            shrunk_url = shortener.shrink_url(target_url)
+
+        return render_template(
+            'index.html',
+            shrunk_url=shrunk_url,
+            target_url=target_url
+        )
+    else:
+        return render_template('index.html')
 
 # Responsible for redirecting the user to the target_url
 # associated with the specified shrunk_url.
